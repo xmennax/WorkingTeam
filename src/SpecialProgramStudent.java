9d69a60cb490cf90468099f5f7fb999daadcb105
xmennax <mennamohamedx304@gmail.com>
2025-08-29 14:58:26 +0300

    SpecialProgramStudent.java

--- src/SpecialProgramStudent.java
+++ src/SpecialProgramStudent.java
@@ -1,4 +1,12 @@
 public class SpecialProgramStudent extends Student{
+    @Override
+    public boolean registerCourse(Course c1){
+        if(this.level<c1.MinStudentLevel){
+            System.out.println("Can't Register");
+            return false;
+        }
+        return super.registerCourse(c1);
+    }
     public float calculateExpenses(){
         int sumOfCreditHours=0;
         for (int i = 0; i < noOfCourses; i++) {
